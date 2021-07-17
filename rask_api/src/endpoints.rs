d4064b64003d02242ff4dac415634e75390b45a7
jrheard <jrheard@cs.stanford.edu>
2021-07-17 10:46:35 -0700

    get thiserror working

--- rask_api/src/endpoints.rs
+++ rask_api/src/endpoints.rs
@@ -2,10 +2,32 @@ use crate::db::DBConn;
 use crate::models::{NewTask, Task};
 use crate::schema::task;
 use diesel::prelude::*;
+use rocket::http::{ContentType, Status};
 use rocket::response::status::Created;
+use rocket::response::{Responder, Response};
 use rocket::serde::json::Json;
 use rocket::{get, post};
 use serde::{Deserialize, Serialize};
+use std::io::Cursor;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum RaskApiError {
+    #[error("Database error")]
+    DatabaseError(#[from] diesel::result::Error),
+}
+
+impl<'r> Responder<'r, 'static> for RaskApiError {
+    fn respond_to(self, _: &'r rocket::Request<'_>) -> rocket::response::Result<'static> {
+        let body = format!("Error: {}", self);
+        let response = Response::build()
+            .status(Status::InternalServerError)
+            .header(ContentType::Plain)
+            .sized_body(body.len(), Cursor::new(body))
+            .finalize();
+        Ok(response)
+    }
+}
 
 #[derive(Deserialize)]
 pub struct TaskJSON {
@@ -23,11 +45,11 @@ pub struct NewTaskResponse {
 }
 
 #[get("/task/<task_id>")]
-pub async fn get_task_by_id(db: DBConn, task_id: i32) -> Option<Json<Task>> {
+pub async fn get_task_by_id(db: DBConn, task_id: i32) -> Result<Json<Task>, RaskApiError> {
     db.run(move |conn| task::table.filter(task::id.eq(task_id)).first(conn))
         .await
         .map(Json)
-        .ok()
+        .map_err(|source| RaskApiError::DatabaseError(source))
 }
 
 #[get("/tasks")]
