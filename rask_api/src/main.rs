d0d1252417345407c2ad87c2b8327432e72650c7
jrheard <jrheard@cs.stanford.edu>
2021-07-16 20:22:50 -0700

    remove Rocket.toml, use figment and dotenv

--- rask_api/src/main.rs
+++ rask_api/src/main.rs
@@ -1,13 +1,16 @@
-// TODO - can i get rid of the macro_use? i seem to need it for the table! usage in schema.rs
 #[macro_use]
 extern crate diesel;
 
 use crate::schema::task;
 use diesel::prelude::*;
 use diesel::PgConnection;
+use dotenv::dotenv;
+use rocket::figment::util::map;
+use rocket::figment::value::{Map, Value};
 use rocket::serde::json::Json;
 use rocket::{get, launch, routes};
 use rocket_sync_db_pools::database;
+use std::env;
 
 pub mod models;
 pub mod schema;
@@ -25,7 +28,15 @@ async fn hello(db: DBConn, task_id: i32) -> Option<Json<models::Task>> {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
+    dotenv().ok();
+
+    let db_url = env::var("DATABASE_URL").unwrap();
+    let db: Map<_, Value> = map! {
+        "url" => db_url.into(),
+        "pool_size" => 10.into()
+    };
+
+    rocket::custom(rocket::Config::figment().merge(("databases", map!["rask_db" => db])))
         .mount("/", routes![hello])
         .attach(DBConn::fairing())
 }
