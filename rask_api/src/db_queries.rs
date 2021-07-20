8aa7485e1705ccfb8451b254731873658e15a285
jrheard <jrheard@cs.stanford.edu>
2021-07-19 19:24:38 -0700

    use diesel's QueryResult instead of rolling our own

--- rask_api/src/db_queries.rs
+++ rask_api/src/db_queries.rs
@@ -3,17 +3,15 @@ use crate::schema::task;
 use diesel::prelude::*;
 use diesel::PgConnection;
 
-type Result<T, E = diesel::result::Error> = std::result::Result<T, E>;
-
-pub fn get_tasks(conn: &PgConnection) -> Result<Vec<Task>> {
+pub fn get_tasks(conn: &PgConnection) -> QueryResult<Vec<Task>> {
     task::table.load(conn)
 }
 
-pub fn get_task_by_id(conn: &PgConnection, task_id: i32) -> Result<Task> {
+pub fn get_task_by_id(conn: &PgConnection, task_id: i32) -> QueryResult<Task> {
     task::table.filter(task::id.eq(task_id)).first(conn)
 }
 
-pub fn create_task(conn: &PgConnection, name: &str) -> Result<Task> {
+pub fn create_task(conn: &PgConnection, name: &str) -> QueryResult<Task> {
     diesel::insert_into(task::table)
         .values(NewTask { name })
         .get_result(conn)
