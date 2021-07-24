fb36bf62a802e5a5c611513a5b17115a3e7ff71b
jrheard <jrheard@cs.stanford.edu>
2021-07-23 20:01:26 -0700

    add first test

--- rask_api/src/main.rs
+++ rask_api/src/main.rs
@@ -28,9 +28,17 @@ async fn run_migrations(rocket: Rocket<Build>) -> Rocket<Build> {
     rocket
 }
 
+fn load_environment_variables() {
+    if env::var("RUST_TESTING").is_ok() {
+        dotenv::from_filename(".env.test").ok();
+    } else {
+        dotenv().ok();
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
-    dotenv().ok();
+    load_environment_variables();
 
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be defined");
     let db: Map<_, Value> = map! {
@@ -51,4 +59,22 @@ fn rocket() -> _ {
         )
         .attach(DBConn::fairing())
         .attach(AdHoc::on_ignite("Diesel Migrations", run_migrations))
+}
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::http::Status;
+    use rocket::local::blocking::Client;
+
+    #[test]
+    fn test_get_tasks_when_no_tasks() {
+        let client = Client::tracked(rocket()).unwrap();
+
+        for url in ["/tasks/all", "/tasks/alive"] {
+            let response = client.get(url).dispatch();
+            assert_eq!(response.status(), Status::Ok);
+            assert_eq!(response.into_string(), Some("{\"tasks\":[]}".to_string()));
+        }
+    }
 }
