b16d630bdc17d6858e90ed964d0a4c8568df71ae
jrheard <jrheard@cs.stanford.edu>
2021-07-25 09:27:34 -0700

    move 'unit tests' to be integration tests

---
+++ rask_api/src/lib.rs
@@ -0,0 +1,62 @@
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate diesel_migrations;
+
+use crate::db::DBConn;
+use dotenv::dotenv;
+use rocket::fairing::AdHoc;
+use rocket::figment::util::map;
+use rocket::figment::value::{Map, Value};
+use rocket::{routes, Build, Rocket};
+use std::env;
+
+mod db;
+mod db_queries;
+pub mod endpoints;
+pub mod models;
+pub mod schema;
+
+/// Runs Diesel migrations as part of `rocket`'s initialization.
+async fn run_migrations(rocket: Rocket<Build>) -> Rocket<Build> {
+    embed_migrations!();
+
+    let conn = DBConn::get_one(&rocket).await.expect("database connection");
+    conn.run(|c| embedded_migrations::run(c))
+        .await
+        .expect("diesel migrations");
+
+    rocket
+}
+
+/// Loads environment variables from .env files.
+pub fn load_environment_variables() {
+    if env::var("RUST_TESTING").is_ok() {
+        dotenv::from_filename(".env.test").ok();
+    }
+    dotenv().ok();
+}
+
+pub fn assemble_rocket() -> Rocket<Build> {
+    load_environment_variables();
+
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be defined");
+    let db: Map<_, Value> = map! {
+        "url" => db_url.into(),
+        "pool_size" => 10.into()
+    };
+
+    rocket::custom(rocket::Config::figment().merge(("databases", map! {"rask_db" => db})))
+        .mount(
+            "/",
+            routes![
+                endpoints::get_tasks,
+                endpoints::get_alive_tasks,
+                endpoints::get_task_by_id,
+                endpoints::create_task,
+                endpoints::complete_task
+            ],
+        )
+        .attach(DBConn::fairing())
+        .attach(AdHoc::on_ignite("Diesel Migrations", run_migrations))
+}
