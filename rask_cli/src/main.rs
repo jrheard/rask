dd6a2a382e2586842651a4ce5ee14b30046bf222
jrheard <jrheard@cs.stanford.edu>
2021-07-28 21:49:02 -0700

    cli hello world: request task with id 1 and serialize the response into json. omg it works

--- rask_cli/src/main.rs
+++ rask_cli/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use rask_lib::models;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::blocking::get("http://localhost:8001/task/1")?.json::<models::Task>()?;
+    println!("{:#?}", resp);
+    Ok(())
 }
