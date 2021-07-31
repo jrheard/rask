1a5a93a56f26bd6b6f0e27abef08bff9e0fe6803
jrheard <jrheard@cs.stanford.edu>
2021-07-31 11:07:28 -0700

    cli: update hello-world to display a list of alive tasks

--- rask_cli/src/main.rs
+++ rask_cli/src/main.rs
@@ -1,7 +1,20 @@
 use rask_lib::models;
 
+const API_ROOT: &str = "http://localhost:8001";
+
+/// Turns an `endpoint` like `task/1` into a full API URL.
+fn make_url(endpoint: &str) -> String {
+    format!("{}/{}", API_ROOT, endpoint)
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::blocking::get("http://localhost:8001/task/1")?.json::<models::Task>()?;
-    println!("{:#?}", resp);
+    let tasks = reqwest::blocking::get(make_url("tasks/alive"))?.json::<Vec<models::Task>>()?;
+
+    println!("Retrieved {} tasks", tasks.len());
+    println!("======================");
+    for task in tasks {
+        println!("{}\t{}", task.id, task.name);
+    }
+
     Ok(())
 }
