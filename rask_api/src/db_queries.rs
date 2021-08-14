74d9335cc4437b50e2f3c5488345591f4da2732d
jrheard <jrheard@cs.stanford.edu>
2021-08-14 12:26:38 -0700

    db_queries: break alive tasks filtering out into a fn

--- rask_api/src/db_queries.rs
+++ rask_api/src/db_queries.rs
@@ -1,6 +1,8 @@
 use diesel::dsl::{any, exists};
+use diesel::pg::Pg;
 use diesel::prelude::*;
 use diesel::select;
+use diesel::sql_types::Bool;
 use diesel::PgConnection;
 use rask_lib::models::{Mode, NewTask, Task, MODE_ACTIVE, MODE_PENDING};
 use rask_lib::schema::api_token;
@@ -10,11 +12,12 @@ pub fn get_tasks(conn: &PgConnection) -> QueryResult<Vec<Task>> {
     task::table.order(task::id).load(conn)
 }
 
+pub fn alive_tasks<'a>() -> Box<dyn BoxableExpression<task::table, Pg, SqlType = Bool> + 'a> {
+    Box::new(task::mode.eq(any(vec![MODE_PENDING.0, MODE_ACTIVE.0])))
+}
+
 pub fn get_alive_tasks(conn: &PgConnection) -> QueryResult<Vec<Task>> {
-    task::table
-        .filter(task::mode.eq(any(vec![MODE_PENDING.0, MODE_ACTIVE.0])))
-        .order(task::id)
-        .load(conn)
+    task::table.filter(alive_tasks()).order(task::id).load(conn)
 }
 
 pub fn get_task_by_id(conn: &PgConnection, task_id: i32) -> QueryResult<Option<Task>> {
