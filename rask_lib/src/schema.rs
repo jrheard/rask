3bc4461ecd607ac41e119c3aaef8d24d29bcd209
jrheard <jrheard@cs.stanford.edu>
2021-08-15 16:07:33 -0700

    WIP, add migration for recurrence templates

--- rask_lib/src/schema.rs
+++ rask_lib/src/schema.rs
@@ -5,6 +5,18 @@ table! {
 }
 
 table! {
+    recurrence_template (id) {
+        id -> Int4,
+        time_created -> Timestamptz,
+        name -> Text,
+        project -> Nullable<Text>,
+        priority -> Nullable<Text>,
+        due -> Date,
+        recurrence_period -> Interval,
+    }
+}
+
+table! {
     task (id) {
         id -> Int4,
         name -> Text,
@@ -13,10 +25,14 @@ table! {
         mode -> Text,
         time_created -> Timestamptz,
         due -> Nullable<Date>,
+        recurrence_template_id -> Nullable<Int4>,
     }
 }
 
+joinable!(task -> recurrence_template (recurrence_template_id));
+
 allow_tables_to_appear_in_same_query!(
     api_token,
+    recurrence_template,
     task,
 );
